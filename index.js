abb9c717f10074d190b93033b1ec7986969a0c31
alfred <alfreddevelops@github.com>
2021-01-09 02:38:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// float value range from 0 - 100
+var x = Math.random() * 101;
+console.log(x);
+// convert to 2 decimals
+var y = Math.round((x + Number.EPSILON) * 100 ) / 100;
+console.log(y)
+
+var betHigh
+
+// function getRange(min, max) {
+// getting float to 2 decimal
+
+
+// log all results
+// append to results-container
+// $("x").appendTo(".results-container");
+
+// chance to win ie: 90%
+// if 0 < y < 90 = win else lose
+
+// chance to win ie: 50%
+// 0 < Y < 50
+
+// autobet use for loop
+
+// get basebet
+// payover returns
+// }
